9b4cab684761337259e3e0657a820d445a69c65d
vipul negi <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-11-12 23:46:25 +0530

    Add files via upload

---
+++ Moudule/modilepart.js
@@ -0,0 +1,6 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+
+
+// this is import for require the any part of th code and function
+module.exports = { add, subtract };
---
+++ Moudule/moudule.js
@@ -0,0 +1,12 @@
+const math = require('buffer')
+
+const math = require('./modilepart');
+
+console.log(math.add(2, 3));  // Output: 5
+console.log(math.subtract(5, 2));  // Output: 3
+
+
+// moudule is th epart of the code or file which are imported be the require keyword
+// what is moudule in node
+
+// In Node.js, a module is a reusable block of code whose existence does not impact other code unless explicitly imported. Think of it as a self-contained piece of functionality that you can export and import in different parts of your application.
